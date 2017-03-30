d6dd220b912dd622e839f655e23f10bfcfb81bc5
pauldambra <pauldambra@gmail.com>
2017-03-30 20:36:10 +0100

    add explicit yarn install to verify script

--- verify.sh
+++ verify.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
+yarn
+
 node_modules/.bin/remark . --use lint --frail
 
-node_modules/.bin/mdspell _posts/*.md \
-  --report \
+node_modules/.bin/mdspell _posts/2014-05-18-promises.md \
   --en-gb \
   --ignore-numbers \
   --ignore-acronyms
