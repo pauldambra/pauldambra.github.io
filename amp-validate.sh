413315f46a2ac62888e3a8bed4cdb8d6685173ba
pauldambra <pauldambra@gmail.com>
2017-05-10 20:11:23 +0100

    try and make the travis scripts exit on errors

--- amp-validate.sh
+++ amp-validate.sh
@@ -2,8 +2,15 @@
 
 set -eu
 
+$AMPDIRECTORY='_site/amp'
+
+if [ ! -d "$AMPDIRECTORY" ]; then
+  echo "$AMPDIRECTORY doesn't exist"
+  exit 1
+fi
+
 npm install -g amphtml-validator
 
-for f in `find _site/amp -type f -name '*.html'`; do
+for f in `find $AMPDIRECTORY -type f -name '*.html'`; do
   amphtml-validator $f
 done
