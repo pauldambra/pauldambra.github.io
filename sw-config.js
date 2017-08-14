dab99c339f4d7f8c83dc217540d361fd22b5dccc
pauldambra <pauldambra@gmail.com>
2017-08-15 00:23:52 +0100

    add svgs to the service worker precache

--- sw-config.js
+++ sw-config.js
@@ -2,7 +2,8 @@ module.exports = {
   staticFileGlobs: [
     '_site/**/**.html',
     '_site/**/**.jpg',
-    '_site/**/**.png'
+    '_site/**/**.png',
+    '_site/**/**.svg'
   ],
   stripPrefix: '_site'
 }
