b5abda6a2e9e1bd1b103838ee79e4f8d0fbf4db6
pauldambra <pauldambra@gmail.com>
2017-08-17 21:00:23 +0100

    turn down the amount of output from image optimisation

--- imageOptimisation.sh
+++ imageOptimisation.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-set -eux
+set -eu
 
 find ./images -name "*.jpg" -type f -exec jpegtran -copy none -optimize -progressive -perfect -outfile {} {} \;
-find ./images/ -name "*.png" -type f -print0 |xargs -n 1 -P 4 -0 optipng -o7
+find ./images/ -name "*.png" -type f -print0 |xargs -n 1 -P 4 -0 optipng -o4 | grep "% decrease"
