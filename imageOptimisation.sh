d345b8886343f7ca47f09601fe38d74d858927fe
pauldambra <pauldambra@gmail.com>
2017-08-17 20:39:12 +0100

    remove silly mistakes from the imageOptimisation script

--- imageOptimisation.sh
+++ imageOptimisation.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-find /images -name "*.jpg" -type f -exec jpegtran -copy none -optimize -progressive -perfect -outfile {} {} \;
-find public/images/ -name "*.png" -type f -print0 |xargs -n 1 -P 4 -0 optipng -o7
+set -eux
+
+find ./images -name "*.jpg" -type f -exec jpegtran -copy none -optimize -progressive -perfect -outfile {} {} \;
+find ./images/ -name "*.png" -type f -print0 |xargs -n 1 -P 4 -0 optipng -o7
