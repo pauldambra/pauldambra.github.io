c227ffda679799d44e952dac9f3888282b50fb38
Travis CI <paul.dambra+ttravis@gmail.com>
2017-10-17 21:51:42 +0100

    Lastest site built on successful travis build 'unknown' auto-pushed to github

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,23 @@
 
 set -eu
 
-DEPLOY_REPO="https://${DEPLOY_BLOG_TOKEN}@github.com/pauldambra/pauldambra.github.io.git"
+TRAVIS_BLOG_TOKEN=${DEPLOY_BLOG_TOKEN:-''}
+
+if [ -z $TRAVIS_BLOG_TOKEN ]
+then
+  DEPLOY_REPO="git@github.com:pauldambra/blog_source.git"
+else
+  DEPLOY_REPO="https://${TRAVIS_BLOG_TOKEN}@github.com/pauldambra/pauldambra.github.io.git"
+fi
 
 echo "deploying changes"
 
-if [ -z "$TRAVIS_PULL_REQUEST" ]; then
+if [ -z "${TRAVIS_PULL_REQUEST:-''}" ]; then
     echo "except don't publish site for pull requests"
     exit 0
 fi  
 
-if [ "$TRAVIS_BRANCH" != "master" ]; then
+if [ "${TRAVIS_BRANCH:-''}" != "master" ]; then
     echo "except we should only publish the master branch. stopping here"
     exit 0
 fi
@@ -21,5 +28,5 @@ git config --global user.name "Travis CI"
 git config --global user.email paul.dambra+ttravis@gmail.com
 git add -A
 git status
-git commit -m "Lastest site built on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to github"
+git commit -m "Lastest site built on successful travis build ${TRAVIS_BUILD_NUMBER:-'unknown'} auto-pushed to github"
 exit `git push $DEPLOY_REPO master:master`
