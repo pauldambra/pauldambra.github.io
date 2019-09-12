ff3e535fcb6da1659160822fb4f79a95a16222b5
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2019-09-12 12:16:07 +0100

    what is failing?

--- imageOptimisation.sh
+++ imageOptimisation.sh
@@ -2,5 +2,8 @@
 
 set -eu
 
+echo "optimising jpegs"
 find ./images -name "*.jpg" -type f -exec jpegtran -copy none -optimize -progressive -perfect -outfile {} {} \;
+
+echo "optimising PNGs"
 find ./images/ -name "*.png" -type f -print0 |xargs -n 1 -P 4 -0 optipng -o4 | grep "% decrease"
