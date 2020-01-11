1dff45c37bea928c2ee2eb958acde0e2d167112e
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2020-01-11 12:01:31 +0000

    refactor ruby that makes weeknote images

--- _plugins/create_week_note_image.rb
+++ _plugins/create_week_note_image.rb
@@ -1,30 +1,27 @@
 require 'webdrivers/chromedriver'
 
-def note_path_for(note, file_extension) 
-  root_dir = note.path.sub(note.relative_path, "")
-  note_path = note.permalink[0..-5]
-
-  root_dir + "_site" + note_path + file_extension
-end
-
 def launch_chrome
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--headless')
   Selenium::WebDriver.for :chrome, options: options
 end
 
-def render_note(html_path, image_path)
-  browser = launch_chrome
-  browser.navigate.to "file:///" + html_path
-  browser.manage.window.resize_to(720, 600)
-  browser.save_screenshot image_path
-  browser.quit
+@browser = launch_chrome
+
+def path_for(note, file_extension) 
+  root_dir = note.path.sub(note.relative_path, "")
+  note_path = note.permalink[0..-5]
+  "#{root_dir}_site#{note_path}#{file_extension}"
 end
 
+def render_note_image(html_path, image_path)
+  @browser.navigate.to "file:///#{html_path}"
+  @browser.manage.window.resize_to(720, 600)
+  @browser.save_screenshot image_path
+end
 
 Jekyll::Hooks.register :weeknotes, :post_write do |note|
-  note_image_path = note_path_for(note, "png")
-  note_html_path = note_path_for(note, "html")
-
-  render_note(note_html_path, note_image_path)
+  html_path = path_for(note, "html")
+  image_path = path_for(note, "png")
+  render_note_image(html_path, image_path)
 end
