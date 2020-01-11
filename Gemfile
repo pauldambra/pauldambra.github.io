2dae109690ba7e35aec4ffb48fb858edc19d5a33
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2020-01-11 00:36:26 +0000

    add weeknotes that generate images for social sharing

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 gem 'github-pages'
 
+gem 'webdrivers'
+
 #for amp generation
 gem 'nokogiri'
 gem 'fastimage'
---
+++ _plugins/create_week_note_image.rb
@@ -0,0 +1,34 @@
+
+require 'webdrivers/chromedriver'
+
+def render_note(html_path, image_path)
+
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  @driver = Selenium::WebDriver.for :chrome, options: options
+  @driver.navigate.to "file:///" + html_path
+
+  @driver.manage.window.resize_to(720, 1000)
+  
+  @driver.save_screenshot image_path
+
+  @driver.quit
+end
+
+
+Jekyll::Hooks.register :weeknotes, :post_write do |note|
+
+  root_dir = note.path.sub(note.relative_path, "")
+  note_path = note.permalink[0..-5]
+
+  note_image_path = root_dir + "_site" + note_path + "png"
+  note_html_path = root_dir + "_site" + note_path + "html"
+
+  p note_html_path
+  p note_image_path
+
+  p '------------'
+
+  render_note(note_html_path, note_image_path)
+
+end
